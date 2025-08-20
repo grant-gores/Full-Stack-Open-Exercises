d503b5267c0d8a79aba41fb845b1a402b1cfa3b0
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-08-20 16:43:33 -0500

    Complete Exercise 1.6 for unicafe

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const Title = (props) => {
+  return <h1>{props.title}</h1>
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Display = (props) => (
+  <p>
+    {props.text} {props.clicks}
+  </p>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    console.log("good clicks", good)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    console.log("neutral clicks", neutral)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    console.log("bad clicks", bad)
+  }
+
+  return (
+    <div>
+      <Title title={"give feedback"}/>
+      <Button onClick={handleGoodClick} text='good'/>
+      <Button onClick={handleNeutralClick} text='neutral'/>
+      <Button onClick={handleBadClick} text='bad'/>
+      <Title title={"statistics"}/>
+      <Display text={"good"} clicks={good}/>
+      <Display text={"neutral"} clicks={neutral}/>
+      <Display text={"bad"} clicks={bad}/>
+    </div>
+  )
+}
+
+export default App
