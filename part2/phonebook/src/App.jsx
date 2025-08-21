ff727f996aa81e0d8f1db34a8a995775a9e52c87
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-08-21 16:37:12 -0500

    Complete Exercise 2.8 for phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react'
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number:'040-1234567'}]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
 const addName = (event) => {
   event.preventDefault()
 
-  const nameObject = { name: newName }
-  const isDuplicate = persons.some(person => person.name === newName);
+  const nameObject = { name: newName, number: newNumber }
+  const isDuplicate = persons.some(person => person.name === newName)
 
   if (isDuplicate) {
     alert(`${newName} is already added to phonebook`)
   } else {
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 }
 
@@ -21,22 +23,23 @@ const addName = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div> name: <input value={newName} onChange={handleNameChange} pattern="[a-zA-Z\s]+" required/> </div>
+        <div> number: <input value={newNumber} onChange={handleNumberChange} pattern="[0-9\-]+" required/> </div>
+        <div> <button type="submit">add</button> </div>
       </form>
       <h2>Numbers</h2>
         <ul>
           {persons.map((person, index) =>
-          <li key={index}>{person.name}</li>
+          <li key={index}>{person.name} {person.number}</li>
           )}
         </ul>
     </div>
