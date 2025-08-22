859c6e680b6159c8536b2d3a74f5ebf9504590f6
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-08-22 13:36:01 -0500

    Complete Exercise 2.9 for phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react'
+
+const Title = (props) => {
+  return (<h2>{props.text}</h2>)
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number:'040-1234567'}]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState("")
 
 const addName = (event) => {
   event.preventDefault()
@@ -19,6 +30,10 @@ const addName = (event) => {
   }
 }
 
+  const handleSearchTerm = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -27,18 +42,23 @@ const addName = (event) => {
     setNewNumber(event.target.value)
   }
 
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <Title text={"Phonebook"}/>
+      <div> filter show with: <input value={searchTerm} onChange={handleSearchTerm} required/> </div>
+      <Title text={"add a new"}/>
       <form onSubmit={addName}>
         <div> name: <input value={newName} onChange={handleNameChange} pattern="[a-zA-Z\s]+" required/> </div>
         <div> number: <input value={newNumber} onChange={handleNumberChange} pattern="[0-9\-]+" required/> </div>
         <div> <button type="submit">add</button> </div>
       </form>
-      <h2>Numbers</h2>
+      <Title text={"Numbers"}/>
         <ul>
-          {persons.map((person, index) =>
+          {filteredPersons.map((person, index) =>
           <li key={index}>{person.name} {person.number}</li>
           )}
         </ul>
