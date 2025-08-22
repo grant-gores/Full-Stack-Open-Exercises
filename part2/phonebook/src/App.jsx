0bf11643dc6e29321e693fe449c5fd58314b0f8a
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-08-22 16:09:01 -0500

    Complete Exercise 2.11 for phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import Title from './components/Title'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState("")
 
-const addName = (event) => {
-  event.preventDefault()
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
-  const nameObject = { name: newName, number: newNumber }
-  const isDuplicate = persons.some(person => person.name === newName)
+  const addName = (event) => {
+    event.preventDefault()
 
-  if (isDuplicate) {
-    alert(`${newName} is already added to phonebook`)
-  } else {
-    setPersons(persons.concat(nameObject))
-    setNewName('')
-    setNewNumber('')
+    const nameObject = { name: newName, number: newNumber }
+    const isDuplicate = persons.some(person => person.name === newName)
+
+    if (isDuplicate) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+      setNewNumber('')
+    }
   }
-}
 
   const handleSearchTerm = (event) => {
     setSearchTerm(event.target.value)
