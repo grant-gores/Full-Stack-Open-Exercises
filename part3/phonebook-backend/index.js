aaf46a86786efe6e50169152891a4e5fffdf80ae
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-08-27 11:20:52 -0500

    Complete exercise 3.7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 let persons = 
@@ -26,6 +27,7 @@ let persons =
     ]
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 // get base url
 app.get('/', (request, response) => {
